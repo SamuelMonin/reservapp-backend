f0cfb065623e8ca9694c52d7039a4b32761d5bec
SamuelMonin <samuel.monin2003@gmail.com>
2023-02-12 20:19:44 +0100

    Fix putItems when id alwready exists.

--- routes/reservation.js
+++ routes/reservation.js
@@ -2,19 +2,22 @@ const router = require('express').Router();
 
 const reservationModel = require('../models/reservation');
 
-router.post('/reservation/put-item', async (req, res) =>{
-    try{
-        const newItem = new reservationModel({
-            _id: req.body.date + 'T' + req.body.startTime + '.000Z',
-            date: req.body.date,
-            name: req.body.name,
-            startTime: req.body.startTime,
-            endTime: req.body.endTime,
-        })
-
-        const saveItem = await newItem.save()
-        res.status(200).json(saveItem);
-    }catch(err){
+router.post('/reservation/put-item', async (req, res) => {
+    try {
+        await reservationModel.updateOne({
+            _id: req.body.date + 'T' + req.body.startTime + '.000Z'
+        },
+            {
+                $set: {
+                    date: req.body.date,
+                    name: req.body.name,
+                    startTime: req.body.startTime,
+                    endTime: req.body.endTime,
+                }
+            },
+            { upsert: true })
+        res.status(200).json({ saved: true });
+    } catch (err) {
         console.log(err)
         res.json(err);
     }
@@ -22,25 +25,25 @@ router.post('/reservation/put-item', async (req, res) =>{
 
 
 router.post('/reservation/get-items', async (req, res) => {
-    try{
+    try {
         const allTodoItems = await reservationModel.find({
             date: req.body.date
         });
         res.status(200).json(allTodoItems)
-    }catch(err){
+    } catch (err) {
         res.json(err);
     }
 })
 
 
-router.delete('/reservation/item/:id', async (req, res)=>{
-    try{
+router.delete('/reservation/item/:id', async (req, res) => {
+    try {
         await reservationModel.findByIdAndDelete(req.params.id);
         res.status(200).json('Item Deleted');
-    }catch(err){
+    } catch (err) {
         res.json(err);
     }
-} )
+})
 
 
 module.exports = router;
